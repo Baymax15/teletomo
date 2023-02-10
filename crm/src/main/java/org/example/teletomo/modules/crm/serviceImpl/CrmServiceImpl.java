efb21d6728757ed67a03fc6834243cf968960c7e
Baymax15 <39257699+Baymax15@users.noreply.github.com>
2023-02-10 14:25:04 +0530

    Remove unused fields from CrmServiceImpl

--- crm/src/main/java/org/example/teletomo/modules/crm/serviceImpl/CrmServiceImpl.java
+++ crm/src/main/java/org/example/teletomo/modules/crm/serviceImpl/CrmServiceImpl.java
@@ -1,12 +1,9 @@
 package org.example.teletomo.modules.crm.serviceImpl;
 
 import org.example.teletomo.modules.crm.service.CrmService;
-import org.example.teletomo.modules.intercoms.service.IntercomsHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -16,12 +13,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @Service
 public class CrmServiceImpl implements CrmService {
 	private static final Logger log = LoggerFactory.getLogger(CrmServiceImpl.class);
-
-	@Autowired
-	IntercomsHandler producer;
-
-	@Autowired
-	JmsTemplate jmsTemplate;
 
 	@Value("${message}")
 	String message;
